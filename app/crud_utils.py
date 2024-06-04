9aa932b4e15e3aca4581121e4f1e7956b361e2c7
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2024-06-04 13:42:29 +0200

    correctly applied the usage of the sqlaclchemy2 queries (sclars and execute)

--- app/crud_utils.py
+++ app/crud_utils.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 from fastapi import HTTPException
-from sqlalchemy import select
+from sqlalchemy import select, insert
 from sqlalchemy.orm import Session
 import app.models as models
 import app.schema as schema
@@ -14,30 +14,24 @@ class DBResponse:
 
 
 def get_author_by_id(db: Session, author_id: int) -> DBResponse:
-    db_author = db.execute(
-        select(models.AuthorModel).where(models.AuthorModel.id == author_id)
-    ).first()
+    db_author = db.scalars(select(models.AuthorModel).where(models.AuthorModel.id == author_id)).first()
     if db_author is None:
         return DBResponse(None, f"Author with id {author_id} not found", 404)
     return DBResponse(db_author)
 
 
 def get_author_by_name(db: Session, author_name_last: str, author_name_first: str) -> DBResponse:
-    db_author = db.execute(
-        select(models.AuthorModel).where(
-            models.AuthorModel.name_last == author_name_last,
-            models.AuthorModel.name_first == author_name_first
-        )
-    ).first()
+    db_author = db.scalars(select(models.AuthorModel).where(
+        models.AuthorModel.name_last == author_name_last,
+        models.AuthorModel.name_first == author_name_first
+    )).first()
     if db_author is None:
         return DBResponse(None, f"Author with name '{author_name_last} {author_name_first}' not found", 404)
     return DBResponse(db_author)
 
 
 def get_authors(db: Session, skip: int = 0, limit: int = 100):
-    return db.execute(
-        select(models.AuthorModel).offset(skip).limit(limit)
-    ).all()
+    return db.scalars(select(models.AuthorModel).offset(skip).limit(limit)).all()
 
 
 def create_author(db: Session, author: schema.AuthorCreate) -> DBResponse:
@@ -72,27 +66,21 @@ def delete_author(db: Session, author_id: int) -> DBResponse:
 
 
 def get_book_by_id(db: Session, book_id: int) -> DBResponse:
-    db_book = db.execute(
-        select(models.BookModel).where(models.BookModel.id == book_id)
-    ).first()
+    db_book = db.scalars(select(models.BookModel).where(models.BookModel.id == book_id)).first()
     if db_book is None:
         return DBResponse(None, f"Book with id {book_id} not found", 404)
     return DBResponse(db_book)
 
 
 def get_book_by_title(db: Session, book_title: str) -> DBResponse:
-    db_book = db.execute(
-        select(models.BookModel).where(models.BookModel.title == book_title)
-    ).first()
+    db_book = db.scalars(select(models.BookModel).where(models.BookModel.title == book_title)).first()
     if db_book is None:
         return DBResponse(None, f"Book with title '{book_title}' not found", 404)
     return DBResponse(db_book)
 
 
 def get_books(db: Session, skip: int = 0, limit: int = 100):
-    return db.execute(
-        select(models.BookModel).offset(skip).limit(limit)
-    ).all()
+    return db.scalars(select(models.BookModel).offset(skip).limit(limit)).all()
 
 
 def create_book(db: Session, book: schema.BookCreate) -> DBResponse:
